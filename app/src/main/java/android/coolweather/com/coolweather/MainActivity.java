744949f21a75f6df5562d5eefa6ee59ad49421f1
YuanXutan <2635846335@qq.com>
2018-03-24 16:18:21 +0800

    First commit

---
+++ app/src/main/java/android/coolweather/com/coolweather/MainActivity.java
@@ -0,0 +1,15 @@
+package android.coolweather.com.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://github.com/YuanXutan/coolweather.git
+    }
+}
